b192a2242c8694f9537b625b2e0717c3726a6706
sanel_selmanovic <sanel.selmanovic@gmail.com>
2017-04-24 01:16:15 -0400

    Fix paperclip config aws region

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Travelgram
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
         # :s3_region => ENV["s3_region"]
-        :s3_region => "us-east-1"
+        :s3_region => ENV["AWS_REGION"]
       }
     }
   end
