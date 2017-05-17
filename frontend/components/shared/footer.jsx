e8049ad671d006744b9c2685e738a9ffccdcd242
Sanel <sanel.selmanovic@gmail.com>
2017-05-17 11:20:32 -0400

    Add footer links

--- frontend/components/shared/footer.jsx
+++ frontend/components/shared/footer.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Footer = ({ children }) => (
   <footer>
-    <h3>&nbsp;</h3>
+    <div className="footer-content">
+
+      <div className="links">
+        <ul className="footer-nav">
+          <li>Sanel Selmanovic</li>
+          <li><a href="http://sanel-selmanovic.us/" target="_blank">portfolio</a></li>
+          <li><a href="https://github.com/s-sanel/" target="_blank">github</a></li>
+          <li><a href="https://www.linkedin.com/in/sanelselmanovic/" target="_blank">linkedin</a></li>
+        </ul>
+
+      </div>
+      <div className="mark">@ 2017 TRAVELGRAM</div>
+    </div>
   </footer>
 );
 
